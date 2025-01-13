c45865ffc367a9cb84c2fb3c7e02abb261f00f9e
will-jerry <117060969+will-jerry@users.noreply.github.com>
2025-01-13 17:13:38 +0800

    Update index.js
    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,24 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
-
+  // ["docker." + CUSTOM_DOMAIN]: dockerHub,
+  // ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
+  // ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
+  // ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
+  // ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
+  // ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
+  // ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
+  // ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  "docker.hostscc.top": "https://registry-1.docker.io",
+  "quay.hostscc.top": "https://quay.io",
+  "gcr.hostscc.top": "https://gcr.io",
+  "k8s-gcr.hostscc.top": "https://k8s.gcr.io",
+  "registry.hostscc.top": "https://registry.k8s.io",
+  "ghcr.hostscc.top": "https://ghcr.io",
+  "cloudsmith.hostscc.top": "https://docker.cloudsmith.io"
+ 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  // ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
 };
 
 function routeByHosts(host) {
